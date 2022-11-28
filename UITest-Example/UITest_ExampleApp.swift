915766acfed4c31d6944f80334e5b3af8dbc117c
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-11-28 23:01:25 +0100

    first commit

---
+++ UITest-Example/UITest_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITest_ExampleApp.swift
+//  UITest-Example
+//
+//  Created by Amjad Oudeh on 28.11.22.
+//
+
+import SwiftUI
+
+@main
+struct UITest_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
